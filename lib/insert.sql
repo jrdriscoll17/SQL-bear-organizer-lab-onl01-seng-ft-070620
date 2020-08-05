c960af507e00c6933ada8b82c8eb34ce56ba3277
Jake Driscoll <jrdriscoll17@gmail.com>
2020-08-05 00:13:59 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Male", "Pink", "Rude", true);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Male", "Pink", "Rude", true);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Male", "Pink", "Rude", true);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Male", "Pink", "Rude", true);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Male", "Pink", "Rude", true);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Male", "Pink", "Rude", true);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Male", "Pink", "Rude", true);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Male", "Pink", "Rude", true);
