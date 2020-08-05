3124db4992724dec83ce7520f981a469c44b1f24
Jake Driscoll <jrdriscoll17@gmail.com>
2020-08-05 00:15:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Male", "Pink", "Rude", true);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "Male", "Pink", "Rude", true);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "Male", "Pink", "Rude", true);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "Male", "Pink", "Rude", true);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "Male", "Pink", "Rude", true);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "Male", "Pink", "Rude", true);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "Male", "Pink", "Rude", true);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES (NULL, 3, "Male", "Pink", "Rude", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Male", "Pink", "Rude", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "Male", "Pink", "Rude", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "Male", "Pink", "Rude", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "Male", "Pink", "Rude", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "Male", "Pink", "Rude", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "Male", "Pink", "Rude", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "Male", "Pink", "Rude", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, "Male", "Pink", "Rude", true);
